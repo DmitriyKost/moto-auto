46023ab2c76d99d18bd7bb29a8b161a1a80efe12
DmitriyKost <dima3588858@gmail.com>
2024-12-22 00:49:51 +0300

    Added pg_cron

--- migrations/0001_initial_schema.sql
+++ migrations/0001_initial_schema.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 CREATE SCHEMA moto_auto;
+CREATE EXTENSION pg_cron;
 
 CREATE TABLE moto_auto.branch (
     branch_id SERIAL PRIMARY KEY,
@@ -237,6 +238,19 @@ CREATE TRIGGER trigger_increment_employee_count
 AFTER INSERT OR UPDATE ON moto_auto.branch_employee
 FOR EACH ROW
 EXECUTE FUNCTION increment_employee_count();
+
+CREATE OR REPLACE FUNCTION expire_bonus_points()
+RETURNS VOID AS $$
+BEGIN
+    IF EXTRACT(MONTH FROM CURRENT_DATE) = 1 AND EXTRACT(DAY FROM CURRENT_DATE) = 1 THEN
+        UPDATE moto_auto.client
+        SET bonus_points = 0;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+SELECT cron.schedule('0 0 1 1 *', 'SELECT expire_bonus_points();');
 
 CREATE ROLE analyst;
 
