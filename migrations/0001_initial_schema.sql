dc7a307a79bd73f3480a07fdac0e88e6832207c5
DmitriyKost <dima3588858@gmail.com>
2024-12-22 17:04:00 +0300

    Minor fixes

--- migrations/0001_initial_schema.sql
+++ migrations/0001_initial_schema.sql
@@ -238,7 +238,7 @@ AFTER INSERT OR UPDATE ON moto_auto.branch_employee
 FOR EACH ROW
 EXECUTE FUNCTION increment_employee_count();
 
-CREATE OR REPLACE FUNCTION expire_bonus_points() -- TODO lauch it in application
+CREATE OR REPLACE FUNCTION expire_bonus_points()
 RETURNS VOID AS $$
 BEGIN
     IF EXTRACT(MONTH FROM CURRENT_DATE) = 1 AND EXTRACT(DAY FROM CURRENT_DATE) = 1 THEN
@@ -351,129 +351,129 @@ CREATE INDEX idx_schedule_branch_id ON moto_auto.schedule(branch_id);
 CREATE INDEX idx_schedule_order_id ON moto_auto.schedule(order_id);
 CREATE INDEX idx_schedule_status ON moto_auto.schedule(status);
 
--- TEST
-INSERT INTO moto_auto.branch (address, phone_number, postal_code, employee_count, city)
-VALUES 
-('123 Main St', '123-456-7890', '12345', 10, 'New York'),
-('456 Elm St', '123-456-7891', '23456', 8, 'Los Angeles'),
-('789 Pine St', '123-456-7892', '34567', 5, 'Chicago');
-
-INSERT INTO moto_auto.users (username, passwordhash, role, branch_id)
-VALUES
-('admin1', 'b56a96a2daa2d0e13bfed0ab5ed2e56fd2152682e8b078515e6058f0f301a059', 'admin', 1),
-('analyst1', 'fb82425cada0f96011ed8e2cdf25679de1168752877d636858dac6b6eb2c7559', 'analyst', 1),
-('manager1', 'b1ba652e4797e6d49d92c0a48dfaaf2ad4168e3ebc028218cab7a959f0985472', 'manager', 1),
-('master1', '68156596aacb0217cba4ee279dfcb95a55eec84c0dcb24ec5d17f97e860b48e1', 'master', 1);
-
-INSERT INTO moto_auto.employee (name, age, position, contact_info, expirience_years, salary, description)
-VALUES
-('John Doe', 35, 'Mechanic', 'john.doe@example.com', 10, 55000, 'Skilled in motorcycle repairs'),
-('Jane Smith', 28, 'Technician', 'jane.smith@example.com', 5, 42000, 'Specializes in diagnostics'),
-('Michael Johnson', 40, 'Manager', 'michael.johnson@example.com', 15, 70000, 'Experienced team leader'),
-('Emily White', 32, 'Customer Service', 'emily.white@example.com', 8, 45000, 'Great at handling clients');
-
-INSERT INTO moto_auto.branch_employee (employee_id, branch_id)
-VALUES
-(1, 1),
-(2, 1),
-(3, 2),
-(4, 3);
-
-INSERT INTO moto_auto.client (name, contact_info, status, bonus_points, total_spent)
-VALUES
-('Alice Cooper', 'alice.cooper@example.com', 'casual', 0, 0),
-('Bob Marley', 'bob.marley@example.com', 'casual', 0, 0),
-('Charlie Brown', 'charlie.brown@example.com', 'casual', 0, 0);
-
-INSERT INTO moto_auto.service (service_name, description) VALUES
-('Oil Change', 'Change of oil for motorcycles'),
-('Brake Repair', 'Replacing or repairing the brake system'),
-('Tire Replacement', 'Changing worn-out tires on bikes');
-
-INSERT INTO moto_auto.service_branch (price, branch_id, service_id)
-VALUES
-(50, 1, 1),
-(50, 2, 1),
-(50, 3, 1),
-(75, 1, 2),
-(75, 2, 2),
-(75, 3, 2),
-(100, 1, 3),
-(100, 2, 3),
-(100, 3, 3);
-
-INSERT INTO moto_auto.spare_part (part_name, description)
-VALUES
-('Brake Pads', 'High-quality brake pads for motorcycles'),
-('Motor Oil', 'Synthetic oil for engine lubrication'),
-('Tires', 'Rubber tires for motorcycles');
-
-INSERT INTO moto_auto.spare_part_branch (part_id, branch_id, stock_quantity, price)
-VALUES
-(1, 1, 20, 30),
-(2, 1, 20, 30),
-(3, 1, 20, 30),
-(1, 2, 20, 30),
-(2, 2, 20, 30),
-(3, 2, 20, 30),
-(1, 3, 20, 30),
-(2, 3, 15, 40),
-(3, 3, 10, 50);
-
-DO $$
-DECLARE
-    i INTEGER;
-BEGIN
-    FOR i IN 1..5000 LOOP
-        INSERT INTO moto_auto.orders (client_id, branch_id, master_id, total_amount, status)
-        VALUES
-            (FLOOR(1 + RANDOM() * 3), FLOOR(1 + RANDOM() * 3), 4, NULL, 'processing');
-    END LOOP;
-END;
-$$;
-
-DO $$
-DECLARE
-    i INTEGER;
-BEGIN
-    FOR i IN 1..5000 LOOP
-        INSERT INTO moto_auto.order_service (order_id, service_id)
-        VALUES
-            (FLOOR(1+RANDOM() * 5000), FLOOR(1 + RANDOM() * 3));
-    END LOOP;
-END;
-$$;
-
-DO $$
-DECLARE
-    i INTEGER;
-BEGIN
-    FOR i IN 1..5000 LOOP
-        INSERT INTO moto_auto.order_service_part (order_service_id, part_id, quantity)
-        VALUES
-            (FLOOR(1 + RANDOM() * 5000), FLOOR(1 + RANDOM() * 3), 1);
-    END LOOP;
-END;
-$$;
-
-DO $$
-DECLARE
-    i INTEGER;
-BEGIN
-    FOR i IN 1..5000 LOOP
-        UPDATE moto_auto.orders
-        SET status = 'finished',
-        completion_date = NOW()
-        WHERE order_id = i
-        AND total_amount IS NOT NULL;
-    END LOOP;
-END;
-$$;
-
-INSERT INTO moto_auto.schedule (client_id, branch_id, order_id, scheduled_datetime, status)
-VALUES
-(1, 1, 1, '2024-12-25 10:00:00', 'confirmed'),
-(2, 2, 2, '2024-12-26 11:00:00', 'pending'),
-(3, 3, 3, '2024-12-27 12:00:00', 'cancelled');
+-- TEST (uncomment if u wanna test data)
+-- INSERT INTO moto_auto.branch (address, phone_number, postal_code, employee_count, city)
+-- VALUES 
+-- ('123 Main St', '123-456-7890', '12345', 10, 'New York'),
+-- ('456 Elm St', '123-456-7891', '23456', 8, 'Los Angeles'),
+-- ('789 Pine St', '123-456-7892', '34567', 5, 'Chicago');
+--
+-- INSERT INTO moto_auto.users (username, passwordhash, role, branch_id)
+-- VALUES
+-- ('admin1', 'b56a96a2daa2d0e13bfed0ab5ed2e56fd2152682e8b078515e6058f0f301a059', 'admin', 1),
+-- ('analyst1', 'fb82425cada0f96011ed8e2cdf25679de1168752877d636858dac6b6eb2c7559', 'analyst', 1),
+-- ('manager1', 'b1ba652e4797e6d49d92c0a48dfaaf2ad4168e3ebc028218cab7a959f0985472', 'manager', 1),
+-- ('master1', '68156596aacb0217cba4ee279dfcb95a55eec84c0dcb24ec5d17f97e860b48e1', 'master', 1);
+--
+-- INSERT INTO moto_auto.employee (name, age, position, contact_info, expirience_years, salary, description)
+-- VALUES
+-- ('John Doe', 35, 'Mechanic', 'john.doe@example.com', 10, 55000, 'Skilled in motorcycle repairs'),
+-- ('Jane Smith', 28, 'Technician', 'jane.smith@example.com', 5, 42000, 'Specializes in diagnostics'),
+-- ('Michael Johnson', 40, 'Manager', 'michael.johnson@example.com', 15, 70000, 'Experienced team leader'),
+-- ('Emily White', 32, 'Customer Service', 'emily.white@example.com', 8, 45000, 'Great at handling clients');
+--
+-- INSERT INTO moto_auto.branch_employee (employee_id, branch_id)
+-- VALUES
+-- (1, 1),
+-- (2, 1),
+-- (3, 2),
+-- (4, 3);
+--
+-- INSERT INTO moto_auto.client (name, contact_info, status, bonus_points, total_spent)
+-- VALUES
+-- ('Alice Cooper', 'alice.cooper@example.com', 'casual', 0, 0),
+-- ('Bob Marley', 'bob.marley@example.com', 'casual', 0, 0),
+-- ('Charlie Brown', 'charlie.brown@example.com', 'casual', 0, 0);
+--
+-- INSERT INTO moto_auto.service (service_name, description) VALUES
+-- ('Oil Change', 'Change of oil for motorcycles'),
+-- ('Brake Repair', 'Replacing or repairing the brake system'),
+-- ('Tire Replacement', 'Changing worn-out tires on bikes');
+--
+-- INSERT INTO moto_auto.service_branch (price, branch_id, service_id)
+-- VALUES
+-- (50, 1, 1),
+-- (50, 2, 1),
+-- (50, 3, 1),
+-- (75, 1, 2),
+-- (75, 2, 2),
+-- (75, 3, 2),
+-- (100, 1, 3),
+-- (100, 2, 3),
+-- (100, 3, 3);
+--
+-- INSERT INTO moto_auto.spare_part (part_name, description)
+-- VALUES
+-- ('Brake Pads', 'High-quality brake pads for motorcycles'),
+-- ('Motor Oil', 'Synthetic oil for engine lubrication'),
+-- ('Tires', 'Rubber tires for motorcycles');
+--
+-- INSERT INTO moto_auto.spare_part_branch (part_id, branch_id, stock_quantity, price)
+-- VALUES
+-- (1, 1, 20, 30),
+-- (2, 1, 20, 30),
+-- (3, 1, 20, 30),
+-- (1, 2, 20, 30),
+-- (2, 2, 20, 30),
+-- (3, 2, 20, 30),
+-- (1, 3, 20, 30),
+-- (2, 3, 15, 40),
+-- (3, 3, 10, 50);
+--
+-- DO $$
+-- DECLARE
+--     i INTEGER;
+-- BEGIN
+--     FOR i IN 1..5000 LOOP
+--         INSERT INTO moto_auto.orders (client_id, branch_id, master_id, total_amount, status)
+--         VALUES
+--             (FLOOR(1 + RANDOM() * 3), FLOOR(1 + RANDOM() * 3), 4, NULL, 'processing');
+--     END LOOP;
+-- END;
+-- $$;
+--
+-- DO $$
+-- DECLARE
+--     i INTEGER;
+-- BEGIN
+--     FOR i IN 1..5000 LOOP
+--         INSERT INTO moto_auto.order_service (order_id, service_id)
+--         VALUES
+--             (FLOOR(1+RANDOM() * 5000), FLOOR(1 + RANDOM() * 3));
+--     END LOOP;
+-- END;
+-- $$;
+--
+-- DO $$
+-- DECLARE
+--     i INTEGER;
+-- BEGIN
+--     FOR i IN 1..5000 LOOP
+--         INSERT INTO moto_auto.order_service_part (order_service_id, part_id, quantity)
+--         VALUES
+--             (FLOOR(1 + RANDOM() * 5000), FLOOR(1 + RANDOM() * 3), 1);
+--     END LOOP;
+-- END;
+-- $$;
+--
+-- DO $$
+-- DECLARE
+--     i INTEGER;
+-- BEGIN
+--     FOR i IN 1..5000 LOOP
+--         UPDATE moto_auto.orders
+--         SET status = 'finished',
+--         completion_date = NOW()
+--         WHERE order_id = i
+--         AND total_amount IS NOT NULL;
+--     END LOOP;
+-- END;
+-- $$;
+--
+-- INSERT INTO moto_auto.schedule (client_id, branch_id, order_id, scheduled_datetime, status)
+-- VALUES
+-- (1, 1, 1, '2024-12-25 10:00:00', 'confirmed'),
+-- (2, 2, 2, '2024-12-26 11:00:00', 'pending'),
+-- (3, 3, 3, '2024-12-27 12:00:00', 'cancelled');
 -- TEST
 COMMIT;
