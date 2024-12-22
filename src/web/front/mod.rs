bee42e4d4273ca364fe6669157190ddd7b68be87
Ivan Nizelnik <iva.nizelnik@gmail.com>
2024-12-22 17:28:25 +0300

    feat(web): manager

--- src/web/front/mod.rs
+++ src/web/front/mod.rs
@@ -1,5 +1,7 @@
 use axum::{routing::get, Router};
-use handlers::{admin_index, login, master_index, order_edit, user_edit};
+use handlers::{
+    admin_index, login, manager_index, master_index, order_edit, order_view, user_edit,
+};
 
 mod handlers;
 mod views;
@@ -7,20 +9,21 @@ mod views;
 pub fn new_front_router() -> Router {
     let view_router = Router::new()
         .route("/user_edit", get(user_edit))
+        .route("/order_view", get(order_view))
         .route("/order_edit", get(order_edit));
 
-    let admin_router = Router::new()
-        .route("/", get(admin_index));
+    let admin_router = Router::new().route("/", get(admin_index));
 
-    let master_router = Router::new()
-        .route("/", get(master_index));
+    let master_router = Router::new().route("/", get(master_index));
 
-    let default_router = Router::new() 
-        .route("/login", get(login));
+    let manager_router = Router::new().route("/", get(manager_index));
+
+    let default_router = Router::new().route("/login", get(login));
 
     Router::new()
         .nest("/admin", admin_router)
         .nest("/master", master_router)
         .nest("/views/", view_router)
+        .nest("/manager", manager_router)
         .nest("/", default_router)
 }
